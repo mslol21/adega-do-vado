5b9aa5e8c9b5647acd53fb277842fb6cbb0f24c5
mslol21 <shitara.massami@gmail.com>
2026-07-16 19:40:42 -0300

    chore: adicionar suporte a navegadores antigos (iPad Mini 1/iOS 9) com plugin-legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    legacy({
+      targets: ['ios >= 9', 'safari >= 9'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    }),
   ],
 })
